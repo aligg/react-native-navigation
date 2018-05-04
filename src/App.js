153555b32cefefe3ceaae86a9dbfc7073cf2b23f
Ali G <ali.glenesk@gmail.com>
2018-05-04 14:58:28 -0500

    Work on authentication with redux

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducers from './reducers'
+import { createStore, applyMiddleware } from 'redux'
 import firebase from 'firebase'
+import ReduxThunk from 'redux-thunk'
+import reducers from './reducers'
 import LoginForm from './components/LoginForm'
 
 class App extends Component {
   componentWillMount() {
-    var config = {
-    apiKey: "AIzaSyCTaI3PfVNRos4N-8zDxjxQSlKcY4gRsfs",
-    authDomain: "manager-300a4.firebaseapp.com",
-    databaseURL: "https://manager-300a4.firebaseio.com",
-    projectId: "manager-300a4",
-    storageBucket: "manager-300a4.appspot.com",
-    messagingSenderId: "626815188064"
-  };
-  firebase.initializeApp(config);
+    const config = {
+    apiKey: 'AIzaSyCTaI3PfVNRos4N-8zDxjxQSlKcY4gRsfs',
+    authDomain: 'manager-300a4.firebaseapp.com',
+    databaseURL: 'https://manager-300a4.firebaseio.com',
+    projectId: 'manager-300a4',
+    storageBucket: 'manager-300a4.appspot.com',
+    messagingSenderId: '626815188064'
+  }
+  firebase.initializeApp(config)
   }
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
     return (
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
       <LoginForm />
     </Provider>
     )
@@ -31,3 +32,6 @@ class App extends Component {
 export default App
 
 // provider tab connects to connect tags and makes sure they can access store and grab redux state
+// applyMiddleware allows us to use ReduxThunk middleware
+// empty {} is optional
+// applyMiddleware() is a store enhancer
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
+import { View, Text } from 'react-native'
 import { connect } from 'react-redux'
-import { emailChanged } from '../actions'
+import { emailChanged, passwordChanged, loginUser } from '../actions'
 import { Card, CardSection, Input, Button } from './common'
 
 
 class LoginForm extends Component {
   onEmailChange(text) {
     this.props.emailChanged(text)
+  }
+
+  onPasswordChange(text) {
+    this.props.passwordChanged(text)
+  }
+
+  onButtonPress() {
+    const { email, password } = this.props
+    this.props.loginUser({ email, password })
+  }
+
+  renderError() {
+    if (this.props.error) {
+      return (
+        <View style={{ backgroundColor: 'white'}}>
+          <Text style={styles.errorTextStyle}>
+            {this.props.error}
+          </Text>
+        </View>
+      )
+    }
   }
 
   render() {
@@ -26,24 +48,39 @@ class LoginForm extends Component {
               secureTextEntry
               label="Password"
               placeholder="Password"
+              onChangeText={this.onPasswordChange.bind(this)}
+              value={this.props.password}
             />
           </CardSection>
 
+          {this.renderError()}
+
           <CardSection>
-            <Button>
+            <Button onPress={this.onButtonPress.bind(this)}>
               Login
             </Button>
-  
           </CardSection>
         </Card>
       )
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    email: state.auth.email
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
   }
 }
 
-export default connect(mapStateToProps, { emailChanged })(LoginForm)
+const mapStateToProps = state => {
+  return {
+    email: state.auth.email,
+    password: state.auth.password,
+    error: state.auth.error
+  }
+}
+
+export default connect(mapStateToProps, {
+  emailChanged, passwordChanged, loginUser
+})(LoginForm)
