ce30c8980b4021214d1d5221ee430996f849f44c
Ali G <ali.glenesk@gmail.com>
2018-05-04 15:20:11 -0500

    finishing auth

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import { connect } from 'react-redux'
 import { emailChanged, passwordChanged, loginUser } from '../actions'
-import { Card, CardSection, Input, Button } from './common'
+import { Card, CardSection, Input, Button, Spinner } from './common'
 
 
 class LoginForm extends Component {
@@ -22,13 +22,24 @@ class LoginForm extends Component {
   renderError() {
     if (this.props.error) {
       return (
-        <View style={{ backgroundColor: 'white'}}>
+        <View style={{ backgroundColor: 'white' }}>
           <Text style={styles.errorTextStyle}>
             {this.props.error}
           </Text>
         </View>
       )
     }
+  }
+
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size='large' />
+    }
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Login
+      </Button>
+    )
   }
 
   render() {
@@ -56,9 +67,7 @@ class LoginForm extends Component {
           {this.renderError()}
 
           <CardSection>
-            <Button onPress={this.onButtonPress.bind(this)}>
-              Login
-            </Button>
+            {this.renderButton()}
           </CardSection>
         </Card>
       )
@@ -77,7 +86,8 @@ const mapStateToProps = state => {
   return {
     email: state.auth.email,
     password: state.auth.password,
-    error: state.auth.error
+    error: state.auth.error,
+    loading: state.auth.error
   }
 }
 
