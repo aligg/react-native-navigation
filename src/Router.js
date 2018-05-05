f2a5de33d49b95ab4312a84fcca5db5444db8536
Ali G <ali.glenesk@gmail.com>
2018-05-05 09:58:37 -0500

    using router and adding additional routes

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Scene, Router } from 'react-native-router-flux'
 import LoginForm from './components/LoginForm'
+import EmployeeList from './components/EmployeeList'
 
 const RouterComponent = () => {
   return (
     <Router>
       <Scene key='root'>
-        <Scene key='login' component={LoginForm} title='Please Login' />
+        <Scene key='login' component={LoginForm} title='Please Login' initial />
+        <Scene key='employeeList' component={EmployeeList} title='Employees' />
       </Scene>
     </Router>
   )
@@ -15,3 +17,4 @@ const RouterComponent = () => {
 export default RouterComponent
 
 // every scene has to have a key
+// initial tells router what to render first (without that, just renders first one)
