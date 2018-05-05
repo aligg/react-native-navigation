19a91bbbdba477457c1b206145b5155fd6702369
Ali G <ali.glenesk@gmail.com>
2018-05-05 18:37:57 -0500

    Employee create complete

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import { Picker } from 'react-native'
+import { Picker, Text } from 'react-native'
 import { connect } from 'react-redux'
 import { employeeUpdate } from '../actions'
-import { Card, CardSection, Input, Button} from './common'
+import { Card, CardSection, Input, Button } from './common'
 
 class EmployeeCreate extends Component {
   render() {
@@ -26,9 +26,9 @@ class EmployeeCreate extends Component {
           />
         </CardSection>
 
-        <CardSection>
+        <CardSection style={{ flexDirection: 'column' }}>
+          <Text style={styles.pickerTextStyle}>Shift</Text>
           <Picker
-            style={{ flex: 1 }}
             selectedValue={this.props.shift}
             onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
           >
@@ -49,6 +49,13 @@ class EmployeeCreate extends Component {
         </CardSection>
       </Card>
     )
+  }
+}
+
+const styles = {
+  pickerTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20
   }
 }
 
