78075d06495ab3f1ad25a41cf5c959551925eea3
Ali G <ali.glenesk@gmail.com>
2018-05-06 15:47:14 -0500

    added sending sms to employee

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,9 +1,10 @@
 import _ from 'lodash'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Communications from 'react-native-communications'
 import EmployeeForm from './EmployeeForm'
 import { Card, CardSection, Button } from './common'
-import { employeeUpdate } from '../actions'
+import { employeeUpdate, employeeSave } from '../actions'
 
 class EmployeeEdit extends Component {
   componentWillMount() {
@@ -14,8 +15,13 @@ class EmployeeEdit extends Component {
 
   onButtonPress() {
     const { name, phone, shift } = this.props
+    this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid })
+  }
 
-    console.log(name, phone, shift)
+  onTextPress() {
+    const { phone, shift } = this.props
+
+    Communications.text(phone, `Your upcoming shift is on ${shift}`)
   }
 
   render() {
@@ -25,6 +31,12 @@ class EmployeeEdit extends Component {
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Save Changes
+          </Button>
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={this.onTextPress.bind(this)}>
+            Text Schedule
           </Button>
         </CardSection>
       </Card>
@@ -37,5 +49,5 @@ const mapStateToProps = (state) => {
   return { name, phone, shift }
 }
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit)
+export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit)
 
--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,6 +1,7 @@
 import {
   EMPLOYEE_UPDATE,
-  EMPLOYEE_CREATE
+  EMPLOYEE_CREATE,
+  EMPLOYEE_SAVE_SUCCESS
 } from '../actions/types'
 
 const INITITAL_STATE = {
@@ -16,6 +17,8 @@ export default (state = INITITAL_STATE, action) => {
       // square braces are not an array, they are for key interpolation
       return { ...state, [action.payload.prop]: action.payload.value }
     case EMPLOYEE_CREATE:
+      return INITITAL_STATE
+    case EMPLOYEE_SAVE_SUCCESS:
       return INITITAL_STATE
     default:
       return state
